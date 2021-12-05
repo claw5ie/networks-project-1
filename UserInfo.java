01ee7d11904f33148440bcda15be1f95ce3bbf7e
poorshadic <up201904872@up.pt>
2021-12-05 20:06:09 +0000

    modified struc

--- UserInfo.java
+++ UserInfo.java
@@ -2,10 +2,12 @@ public class UserInfo {
   public String name;
   public String sala;
   public int state;
+  public SelectionKey = key;
 
-  public UserInfo() {
+  public UserInfo(key) {
     this.name = null;
     this.state = 1;//init
     this.sala = null;
+    this.key = key;
   }
 }
